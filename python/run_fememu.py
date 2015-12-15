7eec6743dbe9c2a4190c857557e329aba004a1c4
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-12-15 10:01:48 -0600

    Keep one driver script, separate config to a .cfg file & introduce a dumb parser --kazu

--- python/run_fememu.py
+++ python/run_fememu.py
@@ -1,43 +1,37 @@
+from fememu_pycfg import apply_config
 import os,sys
 import ROOT as rt
 rt.gSystem.Load("libFEMemulator.so")
 
-fin = sys.argv[1]
+cfg_file = sys.argv[1]
+fin_list = sys.argv[2:len(sys.argv)-2]
 
 #product_name = "opdigit_pmtreadout" # for opdetwaveform
 product_name = "fifo_pmt_xmit"      # for fifo
 
 tree = rt.TChain("%s_tree" % product_name)
-tree.AddFile(fin)
+for f in fin_list:
+    tree.AddFile(f)
 
 entry = 0
 bytes = tree.GetEntry(entry)
 
 config = rt.fememu.FEMBeamTriggerConfig()
-config.fSetTriggerWindow = False
-config.fDiscr0delay = 3
-config.fDiscr3delay = 3
-config.fDiscr0threshold = 5
-config.fDiscr3threshold = 10
-config.fDiscr0precount = 3
-config.fDiscr0deadtime = 6
-config.fDiscr3deadtime = 6
-config.fDiscr0width = 6
-config.fDiscr3width = 6
-config.fMinReadoutTicks = 500
-config.fTriggerWinStartTick = 0
-config.fWindowSize = 103
+apply_config(config,cfg_file)
 
 fememu = rt.fememu.LLInterface(config)
-#fememu._verbosity=rt.fememu.kDEBUG
-fememu._verbosity=rt.fememu.kINFO
+
 product = None
 while bytes>0:
 
     exec('product = tree.%s_branch' % product_name)
 
     out = fememu.Emulate(product)
-    entry += 1
+
+    print '\033[93mEntry',entry,'\033[00m',
+    if out.fire_time_v[0] >= 0:
+        print 'Fire @,',out.fire_time_v[0],
+    print
 
     # If this is fifo type, only use module address 4 (platform) or 5 (test-stand) ... (HACK)
     try:
@@ -45,10 +39,8 @@ while bytes>0:
         
     except Exception:
         pass
-
+    entry += 1
     tree.GetEntry( entry )
-    if entry==10:
-        break
 
 #print digit_array.size()
 
