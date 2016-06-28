4ee44e86a194ac43b4017218950192378f6591ab
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-06-28 11:50:25 -0500

    ups handle added

--- configure_gpvm.sh
+++ configure_gpvm.sh
@@ -1,6 +1,6 @@
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 source /grid/fermiapp/products/uboone/setup_uboone.sh
-setup gcc v4_9_2
+setup gcc v4_9_3
 setup git
-setup cmake v3_0_1
+setup cmake v3_3_2
 source $here/configure.sh
